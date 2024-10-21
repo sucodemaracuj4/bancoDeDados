f578881e296e92711573543507bf924fa5a6424e
su <111879114+sucodemaracuj4@users.noreply.github.com>
2024-10-21 11:01:54 -0300

    Add files via upload

--- quantidadeDeUsuarioPorRede.js
+++ quantidadeDeUsuarioPorRede.js
@@ -11,7 +11,37 @@ async function quantidadeUsuariosPorRede() {
             type: 'bar'
         }
     ]
-    
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('bg-color'),
+        title:{
+            text:'Redes sociais com mais usuários',
+            x:0,
+            font:{
+                color: getCSS(--primary-color),
+                size:30,
+                font: getCSS('--font')
+            }
+        },
+        xaxis:{
+            tickfont: tickConfig,
+            title:{
+                text: 'Nome das redes',
+                font:{
+                    color: getCSS('--secondary-color')
+                }
+            }
+        },
+        yaxis:{
+            tickfont:tickConfig,
+            title:{
+                text:'Bilhões de usuários ativos',
+                font:{
+                    color:getCSS('--secondary-color')
+                }
+            }
+        }
+    }
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
